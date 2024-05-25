00714c752a3f454d7ed72173c98dd5486594e41b
Philipp <p.eistrach@gmail.com>
2024-05-25 17:26:29 +0200

    feat: timer section on game cup page

--- app/routes/game-cup+/index.tsx
+++ app/routes/game-cup+/index.tsx
@@ -2,11 +2,13 @@ import Hero from "~/routes/_index+/components/hero";
 import FaqSection from "~/routes/game-cup+/components/sections/faq-section";
 import Footer from "~/components/layout/footer";
 import ParticipantsSection from "~/routes/game-cup+/components/sections/participants-section";
+import TimerSection from "~/routes/game-cup+/components/sections/timer-section";
 
 export default function GameCupPage() {
   return (
     <div className="flex flex-col gap-20 bg-white">
       <Hero />
+      <TimerSection />
       <ParticipantsSection />
       <FaqSection />
       <Footer />
