fd33b313e0add08dc0e238c112a203d18d17fca5
Philipp <p.eistrach@gmail.com>
2024-05-25 17:29:17 +0200

    refactor: game cup meta tags

--- app/routes/game-cup+/index.tsx
+++ app/routes/game-cup+/index.tsx
@@ -3,6 +3,18 @@ import FaqSection from "~/routes/game-cup+/components/sections/faq-section";
 import Footer from "~/components/layout/footer";
 import ParticipantsSection from "~/routes/game-cup+/components/sections/participants-section";
 import TimerSection from "~/routes/game-cup+/components/sections/timer-section";
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Space Chunks Game Cup" },
+    {
+      name: "description",
+      content:
+        "Join the Space Chunks Game Cup and compete against other players to win prizes!",
+    },
+  ];
+};
 
 export default function GameCupPage() {
   return (
