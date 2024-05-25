02d8b8bceb1a389d46cf42b491676b97d28a3615
Philipp <p.eistrach@gmail.com>
2024-05-25 23:36:32 +0200

    refactor: improve faq section

--- app/routes/game-cup+/components/sections/faq-section.tsx
+++ app/routes/game-cup+/components/sections/faq-section.tsx
@@ -9,30 +9,24 @@ import {
 
 const faqItems = [
   {
-    question: "How can I become part of Space Chunks?",
+    question: "Can I stream during the development phase?",
     answer:
-      "Yes you can! Just contact us on Discord and we'll get back to you.",
+      "Yes, you can stream your development process. However, we recommend that you do not share all of your ideas and progress to keep the competition spicy.",
   },
   {
-    question: "How is the studio financed?",
+    question: "Can I put my games on my network after the competition?",
     answer:
-      "We are currently self-financed, that means we don't have any external investors.",
+      "Yes, you can put your games on your network after the competition. We encourage you to share your creations with the community.",
   },
   {
-    question: "What projects will there be in the future?",
+    question: "What is the deadline for my application",
     answer:
-      "We are planning to host more events and competitions in the future, besides that we are working some secret projects. Stay tuned!",
+      "The deadline for your application is the 1st of July. Make sure to submit all before the deadline to participate in the competition.",
   },
   {
-    question:
-      "What does a partnership look like and how can I become a partner?",
+    question: "Can I participate when I am not a developer?",
     answer:
-      "We are always looking for partners who share our vision. Just contact us and we'll get back to you.",
-  },
-  {
-    question: "How can I support Space Chunks?",
-    answer:
-      "You can support us by participating in our events, competitions, or by becoming a partner.",
+      "You can participate in the competition even if you are not a developer, but you need to have a team with at least one developer. You can also participate as a team of developers.",
   },
 ];
 
