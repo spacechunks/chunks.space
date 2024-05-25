683263aa1f2d257f9c0599e439ac9b7600f5d036
Philipp <p.eistrach@gmail.com>
2024-05-25 20:40:20 +0200

    feat: og images

--- app/routes/_index+/index.tsx
+++ app/routes/_index+/index.tsx
@@ -7,11 +7,42 @@ import GameCupSection from "~/routes/_index+/components/sections/game-cup-sectio
 import TeamSection from "~/routes/_index+/components/sections/team-section";
 import FaqSection from "~/routes/_index+/components/sections/faq-section";
 import ContactSection from "~/routes/_index+/components/sections/contact-section";
+import ogTwitter from "~/assets/images/og-twitter.png";
+import ogFacebook from "~/assets/images/og-facebook.png";
 
 export const meta: MetaFunction = () => {
   return [
     { title: "Space Chunks" },
-    { name: "description", content: "Welcome to Space Chunks!" },
+    {
+      name: "description",
+      content:
+        "Space Chunks a team of developers, designers, and event organizers who are passionate about creating unique experiences for Minecraft. Our goal is to have fun and create something that we can all be proud of.",
+    },
+    // Facebook Open Graph Meta Tags
+    { property: "og:title", content: "Space Chunks" },
+    {
+      property: "og:description",
+      content:
+        "Space Chunks a team of developers, designers, and event organizers who are passionate about creating unique experiences for Minecraft. Our goal is to have fun and create something that we can all be proud of.",
+    },
+    { property: "og:image", content: ogFacebook },
+    { property: "og:image:alt", content: "Space Chunks" },
+    { property: "og:url", content: "https://chunks.space" },
+    { property: "og:type", content: "website" },
+    // Twitter Card Meta Tags
+    { name: "twitter:card", content: "summary_large_image" },
+    { name: "twitter:site", content: "@spacechunksteam" },
+    { name: "twitter:title", content: "Space Chunks" },
+    {
+      name: "twitter:description",
+      content:
+        "Space Chunks a team of developers, designers, and event organizers who are passionate about creating unique experiences for Minecraft. Our goal is to have fun and create something that we can all be proud of.",
+    },
+    { name: "twitter:image", content: ogTwitter },
+    // Discord Meta Tags
+    { property: "og:site_name", content: "Space Chunks" },
+    // General Meta Tags (these get picked up by various platforms including Discord)
+    { property: "og:locale", content: "en_US" },
   ];
 };
 
